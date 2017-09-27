48c588d7b389daa0a1d336d3332253f965cbf322
Maquiz <maximilian.bolling@gmail.com>
2017-09-26 17:48:39 -0700

    Mapped hw 1
    Mapped out base function of hw 1

---
+++ 9_26 Homework/main.cpp
@@ -0,0 +1,25 @@
+// Maximilian Bolling
+// mbolling@horizon.csueastbay.edu
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int[] col1,col2;
+    string[] invalid;
+    float avg1;
+    int currentInt = 0, highest2 =0, rows = 0, divisor = 0;
+    //While name is not correct
+    cout << "Please input the filename:" << endl;
+    cin>> name;
+    //if name is correct loop through rows of data
+    //if first col data is an int add to col1, add to avg1 and divisor++
+    //else add to invalid
+    //2nd col if int add to col2,
+    //if currentInt > highest2, highest2 = currentInt
+    //else add to invalid
+    //rows++
+    return 0;
+}
