85066719264da2ef7583d4de5c5cdc2c0af5dbe0
Maquiz <maximilian.bolling@gmail.com>
2017-09-27 23:00:12 -0700

    Added Data File
    Can now take in stream of data and puts it into one large string

--- 9_26 Homework/main.cpp
+++ 9_26 Homework/main.cpp
@@ -2,6 +2,7 @@
 // mbolling@horizon.csueastbay.edu
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
 double avg(int col[]);
@@ -15,28 +16,50 @@ int main()
     int test [] = {7,8000,99999,11,100000};
     string tests[] = {"asdasd", "blah", "goods"};
     string invalid[10];
+    string line,tester;
     double avg1;
     int currentInt = 0, highest2 =0, rows = 0, divisor = 0;
+
+    //===================================Input=================================================
     //While name is not correct
     cout << "Please input the filename:" << endl;
     cin>> name;
+
+    ifstream myfile ("DataFile.txt");
+    if(myfile.is_open()){
+            while(getline(myfile,line)){
+                line = (line+"\n");
+                tester += line ;
+                //cout<<line<<endl;
+            }
+            myfile.close();
+    }else{
+
+         cout<<"cantfind"<<endl;
+        }
+
+
     //once we have the correct file we pull all data into one string if possible
-
-    readInvalid(tests);
-    //Avg
-    cout<<std::fixed;
-    cout<<std::setprecision(4)<<avg(test)<<endl;;
-
-    cout<<high(test)<<endl;
     //if name is correct loop through rows of data
-
-
     //if first col data is an int add to col1, add to avg1 and divisor++
     //else add to invalid
     //2nd col if int add to col2,
     //if currentInt > highest2, highest2 = currentInt
     //else add to invalid
     //rows++
+
+    //===================================Output=================================================
+    cout<<tester<<endl;
+    //Avg C1
+    cout<<std::fixed;
+    cout<<"The average value of all numbers in the first column: "<<std::setprecision(4)<<avg(test)<<endl;;
+    //High C2
+    cout<<"The largest value of all numbers in the second column: "<<high(test)<<endl;
+    //Total Rows in file
+    cout<<"The total number of rows in the file is: "<<rows<<endl;
+    //Invalid numbers
+    readInvalid(tests);
+
     return 0;
 }
 
@@ -66,6 +89,7 @@ int test = 0, cur = 0;
 //Potentially references to all arrays
 void conversion(string s,int c1[],int c2[],string inv[]){
 //grab first section up to ','
+
 //test if it is an int or string
 //if its an int add to c1 else add to inv
 //grab next set of string to '/n'
