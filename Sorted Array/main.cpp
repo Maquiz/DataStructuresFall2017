6625d7d0fa0f44d3afc8fc073cb6bc095767426f
Maquiz <maximilian.bolling@gmail.com>
2017-10-12 15:47:12 -0700

    Comparing Two cards implemented
    -CompareTo Returns True if card this is called from is greater than
    parameter card
    -printDeck now prints all  non null items in deck array in ascending
    order

--- Sorted Array/main.cpp
+++ Sorted Array/main.cpp
@@ -9,32 +9,38 @@ private:
     string suit, ranking;
     int sInt=0,rInt=0;
 
-
-
 public:
     void printCard(){cout<<suit<<ranking<<endl;}
     void printCardInt(){cout<<sInt<<rInt<<endl;}
+
+    //Constructors
     card(string s,string r){
-    setSuit(s);
-    setRank(r);
-    convert(s,r);
-}
-    card(){
-    setSuit("NULL");
+        setSuit(s);
+        setRank(r);
+        convert(s,r);
     }
 
-    void setSuit(string s){suit = s;}
-    string getSuit(){return suit;}
+    card(){
+        setSuit("NULL");
+        setRank("NULL");
+    }
 
-    void setRank(string r){ranking = r;}
-    string getRank(){return ranking;}
+    //Returns True if card this is called from is greater than parameter card c
+    bool ComparedTo(card c){
+        if(c.getRInt()>getRInt()){
+            return false;
+        }else if(c.getRInt()<getRInt()){
+           return true;
+        }else if(c.getRInt() == getRInt()){
+            if(c.getSInt()>getSInt()){
+                return false;
+            }else{
+                return true;
+                }
+            }
+        }
 
-    void setSInt(int s){sInt = s;}
-    int getSInt(){return sInt;}
-
-    void setRInt(int r){rInt = r;}
-    int getRInt(){return rInt;}
-
+     //Converts string values of Suit and Ranking into int values SInt and RInt
     void convert(string s,string r){
         //Suit If
         if(getSInt() == 0)
@@ -59,11 +65,25 @@ public:
             else if(r=="A") setRInt(14);
     }
 
+    //Getter's and Setter's
+    void setSuit(string s){suit = s;}
+    string getSuit(){return suit;}
+
+    void setRank(string r){ranking = r;}
+    string getRank(){return ranking;}
+
+    void setSInt(int s){sInt = s;}
+    int getSInt(){return sInt;}
+
+    void setRInt(int r){rInt = r;}
+    int getRInt(){return rInt;}
+
 };
+
 //===================================Prototypes================================================
-void putItem(card d[], card c); //insert card into deck in ascending order
-void deleteItem(card d[], card c); //delete card from deck
-void getItem(card d[], card c); //test if card is in deck
+void putItem(card d[], card c, int length); //insert card into deck in ascending order
+void deleteItem(card d[], card c, int length); //delete card from deck
+void getItem(card d[], card c, int length); //test if card is in deck
 void printDeck(card d[]);   //print all cards in deck
 
 
@@ -75,6 +95,7 @@ int main()
     deck = new card[20];
     string data,line;
     int it1 = 0, it2 = 0;
+    int length = 0;
     string name = "DataFile.txt";
     //===================================Input=================================================
     //input data file into one string
@@ -92,35 +113,59 @@ int main()
     //1st 20 items = inputCards1 ;
     for(int i = 0;i<20;i++){
             //if(){
-
           //  }
-
     }
     // Go to nextline = delete cards
     // go to next line = inputcards2
     //go to next line = testCards;
 
-    deck[0]= card("H","7");
+    //================================TESTING AREA=============================================
+    deck[0]= card("H","2");
+    deck[1]= card("D","8");
+    deck[2]= card("S","A");
+    deck[3]= card("C","7");
+    deck[4]= card("D","J");
+    printDeck(deck);
+    cout<<"Print Deck Test^^^^^"<<endl<<endl;
 
     card g("H","10");
+
+    card a("C","2");
+    card t("C","10");
+    card b("S","10");
+    card c("D","A");
     g.printCard();
     g.printCardInt();
+    cout<<"Should return 1: "<<g.ComparedTo(a)<<endl;
+    cout<<"Should return 1: "<<g.ComparedTo(t)<<endl;
+    cout<<"Should return 0: "<<g.ComparedTo(b)<<endl;
+    cout<<"Should return 0: "<<g.ComparedTo(c)<<endl;
 
-    card f("S","A");
-
-    f.printCard();
-    f.printCardInt();
+    //========================!!!!!!!!!!END OF FUNTION!!!!!!!!=================================
     return 0;
 }
 //===================================Function Definitions======================================
 
-//Not complete
-/*
-void addToArray(int t[], int j){
-    int length = 0;
-    int pos = compareTo(t,j);
-    if(pos==0) t[pos] = j;
-}*/
+void putItem(card d[], card c, int length){
+    int l = length;
+    int pos = 0;
+    while(pos<=l){
+        //card is larger than the one in the deck
+        if(c.ComparedTo(d[pos])){
+            pos++;
+        }else{
+            //put card at pos move all other objects down one spot starting at bottom
+        }
+    }
+}
+
+void printDeck(card d[]){
+    int it = 0;
+    while(d[it].getSuit() != "NULL"){
+        d[it].printCard();
+        it++;
+    }
+}
 //Returns position of the same  item;
 /*
 int compareTo(int t[],int j){
